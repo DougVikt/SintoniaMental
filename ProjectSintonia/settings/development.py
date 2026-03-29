9df188d65eab018a8de0fdd209599538bbac0d8c
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2026-03-29 20:34:42 -0300

    feat(configuração): adicionar variaveis do banco via decouple

--- ProjectSintonia/settings/development.py
+++ ProjectSintonia/settings/development.py
@@ -1,21 +1,23 @@
 from .origin_base import *
+from decouple import config
 
 """
 Configurações específicas para o ambiente de desenvolvimento.
+Specific settings for the development environment.
+
 """
 
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    
-]
+ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': config('DB_ENGINE' , default ='django.db.backends.postgresql'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
- 
--- ProjectSintonia/settings/production.py
+++ ProjectSintonia/settings/production.py
@@ -1,20 +1,23 @@
 from .origin_base import *
+from decouple import config
 
 """
 Configurações específicas para o ambiente de produção.
+Specific configurations for the production environment
+
 """
 
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    'sintonia-mental.onrender.com',
-    
-]
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
 DATABASES = {
     'default': {
-        'ENGINE': '',
-        'NAME': BASE_DIR / '',
+        'ENGINE': config('DB_ENGINE' , default ='django.db.backends.postgresql'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
- 
