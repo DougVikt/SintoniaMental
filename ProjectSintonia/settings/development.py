c713b9c24626e4d187d97d045029cc7ed94e6ccc
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2026-04-05 22:06:13 -0300

    feat: comentar caminho para banco teste

--- ProjectSintonia/settings/development.py
+++ ProjectSintonia/settings/development.py
@@ -11,13 +11,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-DATABASES = {
-    'default': {
-        'ENGINE': config('DB_ENGINE' , default ='django.db.backends.postgresql'),
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': config('DB_ENGINE' , default ='django.db.backends.postgresql'),
+#         'NAME': config('DB_NAME'),
+#         'USER': config('DB_USER'),
+#         'PASSWORD': config('DB_PASSWORD'),
+#         'HOST': config('DB_HOST'),
+#         'PORT': config('DB_PORT'),
+#     }
+# }
