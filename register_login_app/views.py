e81bebc171fd09aea2b5eeaa13160d8ecce85095
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2026-04-17 21:51:23 -0300

    feat: retirar nav pela variavel
    - add_nav variavel na base global html , valor false remove o nav

--- register_login_app/views.py
+++ register_login_app/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 
 def register_login(request):
-    return render(request, 'register_login_app/register_login.html')
+    return render(request, 'register_login_app/register_login.html' , 
+                  {
+                  "add_nav":False,
+                  
+                  })
