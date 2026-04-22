ab84a8cb01d4606ede974d737fbdbb435902b9e8
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2026-04-22 17:17:15 -0300

    fix: erro ao criar banco externo

--- ProjectSintonia/settings/production.py
+++ ProjectSintonia/settings/production.py
@@ -11,13 +11,13 @@ DEBUG = False
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
-DATABASES = {
-    'default': {
-        'ENGINE': config('DB_ENGINE' , default ='django.db.backends.postgresql'),
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': config('DB_ENGINE' , default ='django.db.backends.postgresql'),
+#         'NAME': config('DB_NAME'),
+#         'USER': config('DB_USER'),
+#         'PASSWORD': config('DB_PASSWORD'),
+#         'HOST': config('DB_HOST'),
+#         'PORT': config('DB_PORT'),
+#     }
+# }
