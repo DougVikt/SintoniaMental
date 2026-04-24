2bdc965f1af9b3f2703a6ee79f865a9dde00fbbb
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2026-04-24 18:00:16 -0300

    feat: adicionar models
    - adicionado model para o faq com categoria separada e estrutura de listagem do textfield
    - adicionado model de depoimentos no app de relacionamentos

--- faq_app/models.py
+++ faq_app/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class CategoryFaq(models.Model):
+    name = models.CharField( max_length=50 , verbose_name='nome da categoria')
+    icon_class = models.CharField( max_length=50 ,help_text='icone do bootstrap')
+    
+    class Meta:
+        db_table = 'category_faq'
+        verbose_name = 'Categoria da pertunta'
+        verbose_name_plural = 'Categorias das pertuntas'
+        
+    def __str__(self):
+        return self.nome
+    
+    
+class QuestionAnswer(models.Model):
+    category = models.ForeignKey(CategoryFaq, on_delete=models.CASCADE , help_text='categoria da pergunta')
+    question = models.CharField( max_length=200 ,verbose_name= 'pergunta')
+    answer = models.TextField(verbose_name='resposta')
+    list_answer = models.TextField(help_text='a cada linha e item da lista')
+    
+    class Meta:
+        db_table = 'question_answer'
+        verbose_name = 'Pergunta e resposta'
+        verbose_name_plural = 'Perguntas e respostas'
+        
+    
+    def __str__(self):
+        return self.question
+    
+    @property
+    def get_list(self):
+        """Transforma o texto em lista para o template automaticamente"""
+        if self.list_answer:
+            # splitlines remove espaços extras e lida com quebras de linha (\n ou \r\n)
+            return [item.strip() for item in self.list_answer.splitlines() if item.strip()]
+        return []
+
+    def __str__(self):
+        return self.question
--- relacion_user_app/models.py
+++ relacion_user_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.core.validators import MinValueValidator , MaxValueValidator
 
 # Create your models here.
+class Testimonial(models.Model):
+    patient = models.ForeignKey("register_login_app.Patient" ,on_delete=models.CASCADE , related_name="testimonials")
+    expert = models.ForeignKey("register_login_app.Experts", on_delete=models.CASCADE , related_name="testimonials")
+    note = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    comment = models.TextField(default="Não quero comentar" , blank=True)
+    
+    class Meta:
+        db_table = 'testimonials'
+        verbose_name = 'Depoimento'
+        verbose_name_plural = 'Depoimentos'
+    
+    def __str__(self):
+        return f'{self.expert.user.username}: {self.note}'
+    
