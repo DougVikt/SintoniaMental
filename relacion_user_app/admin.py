ad943222d12e88b01fbc8b3811bebad5fe0c37d3
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2026-05-10 20:51:36 -0300

    feat: personalizar panel admin

--- relacion_user_app/admin.py
+++ relacion_user_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Testimonial
 
-# Register your models here.
+
+@admin.register(Testimonial)
+class AdminTestimonial(admin.ModelAdmin):
+    list_display = ["id" , "patient" , "expert"]
