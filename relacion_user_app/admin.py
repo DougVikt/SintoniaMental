c92d4b4a6c529e506b73ddcf45e2a2464b556075
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2026-05-11 21:31:51 -0300

    feat: registrar consult no admin
    - separação com herança para os registros de testimonial e consult

--- relacion_user_app/admin.py
+++ relacion_user_app/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from .models import Testimonial
+from .models import Testimonial, Consult
 
 
+class AdminBase(admin.ModelAdmin):
+    list_display = ["id" , "patient" , "expert"] 
+    raw_id_fields = ['patient' , 'expert']
+    
+    
 @admin.register(Testimonial)
-class AdminTestimonial(admin.ModelAdmin):
-    list_display = ["id" , "patient" , "expert"]
+class AdminTestimonial(AdminBase):
+    pass
+@admin.register(Consult)
+class AdminConsult(AdminBase):
+    pass 
