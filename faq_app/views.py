3a94351bc203e5e860f9c4221c3b37936aac205a
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2026-05-17 21:51:38 -0300

    feat: adicionar logica na view para receber votos

--- faq_app/views.py
+++ faq_app/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from django.db.models import Count
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+import json
 from .models import CategoryFaq ,QuestionAnswer
 
 def faq(request):
@@ -11,3 +14,20 @@ def faq(request):
         'faq':faq,
         'total_quest':total_quest
     })
+
+
+
+@require_POST
+def votar(request, pk):
+    data     = json.loads(request.body)
+    useful     = data.get('useful')  # True ou False
+    not_useful = data.get('not_useful')
+    question = get_object_or_404(QuestionAnswer, pk=pk)
+
+    if useful:
+        question.useful += 1
+    if not_useful:
+        question.not_useful += 1
+    question.save()
+
+    return JsonResponse({'ok': True})
